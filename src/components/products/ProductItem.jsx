b25d710358804f52be42a563b96093b5e857de39
Beau <pg.beau@outlook.com>
2023-04-27 08:38:39 +0800

    add Card

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -2,7 +2,7 @@
  * @Author: Beau pg.beau@outlook.com
  * @Date: 2023-04-27 03:36:36
  * @LastEditors: Beau pg.beau@outlook.com
- * @LastEditTime: 2023-04-27 07:26:05
+ * @LastEditTime: 2023-04-27 08:07:56
  * @FilePath: \workspace\ez-swap\src\components\products\ProductItem.jsx
  * @Description:
  *
@@ -11,64 +11,67 @@
 
 import Image from 'next/image';
 import classes from './ProductItem.module.scss';
+import Card from '../ui/Card';
 
 function ProductItem({ value }) {
   return (
     <ul className={classes.products}>
-      <li>
-        <img src="/image/image 3.png" alt=""></img>
+      <Card>
+        <li>
+          <img src="/image/image 3.png" alt=""></img>
 
-        <div className={classes.container}>
-          <div className={classes.title}>{value.title}</div>
+          <div className={classes.container}>
+            <div className={classes.title}>{value.title}</div>
 
-          <div className={classes.footer}>
-            <div>
-              <span>Listings:{'\u00A0'}</span>
-              <Image
-                src="/svg/eth-icon.svg"
-                alt="eth-icon"
-                width={16}
-                height={16}
-                className={classes.ethIcon}
-              />
-              {value.listings}
-            </div>
-            <div>
-              <span>Floor Price:{'\u00A0'}</span>
-              <Image
-                src="/svg/eth-icon.svg"
-                alt="eth-icon"
-                width={16}
-                height={16}
-                className={classes.ethIcon}
-              />
-              {value['floor-price']}
-            </div>
-            <div>
-              <span>Best offer:{'\u00A0'}</span>
-              <Image
-                src="/svg/eth-icon.svg"
-                alt="eth-icon"
-                width={16}
-                height={16}
-                className={classes.ethIcon}
-              />
-              {value['best-offer']}
-            </div>
-            <div>
-              <span>Volume:{'\u00A0'}</span>
-              <Image
-                src="/svg/eth-icon.svg"
-                alt="eth-icon"
-                width={16}
-                height={16}
-                className={classes.ethIcon}
-              />
-              {value.volume}
+            <div className={classes.footer}>
+              <div>
+                <span>Listings:{'\u00A0'}</span>
+                <Image
+                  src="/svg/eth-icon.svg"
+                  alt="eth-icon"
+                  width={16}
+                  height={16}
+                  className={classes.ethIcon}
+                />
+                {value.listings}
+              </div>
+              <div>
+                <span>Floor Price:{'\u00A0'}</span>
+                <Image
+                  src="/svg/eth-icon.svg"
+                  alt="eth-icon"
+                  width={16}
+                  height={16}
+                  className={classes.ethIcon}
+                />
+                {value['floor-price']}
+              </div>
+              <div>
+                <span>Best offer:{'\u00A0'}</span>
+                <Image
+                  src="/svg/eth-icon.svg"
+                  alt="eth-icon"
+                  width={16}
+                  height={16}
+                  className={classes.ethIcon}
+                />
+                {value['best-offer']}
+              </div>
+              <div>
+                <span>Volume:{'\u00A0'}</span>
+                <Image
+                  src="/svg/eth-icon.svg"
+                  alt="eth-icon"
+                  width={16}
+                  height={16}
+                  className={classes.ethIcon}
+                />
+                {value.volume}
+              </div>
             </div>
           </div>
-        </div>
-      </li>
+        </li>
+      </Card>
     </ul>
   );
 }
--- src/components/products/ProductsList.jsx
+++ src/components/products/ProductsList.jsx
@@ -5,7 +5,7 @@ import classes from './ProductsList.module.scss';
  * @Author: Beau pg.beau@outlook.com
  * @Date: 2023-04-27 02:05:29
  * @LastEditors: Beau pg.beau@outlook.com
- * @LastEditTime: 2023-04-27 06:18:56
+ * @LastEditTime: 2023-04-27 08:02:55
  * @FilePath: \workspace\ez-swap\src\components\products\ProductsList.jsx
  * @Description:
  *
@@ -15,7 +15,7 @@ function ProductsList({ nftData }) {
   return (
     <>
       <header className={classes.header}>Trending</header>
-      <main>
+      <main className={classes.main}>
         {Object.values(nftData).map((value) => (
           <ProductItem value={value} key={value.title}></ProductItem>
         ))}
